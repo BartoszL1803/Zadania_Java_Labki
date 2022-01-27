f98df3d927df43c690977a0104b370cb0c51105d
BartoszL1803 <bartek.lissowski@gmail.com>
2022-01-27 20:28:02 +0100

    Zadanie 2: get yourself a car

---
+++ src/com/company/Car.java
@@ -0,0 +1,15 @@
+package com.company;
+
+public class Car {
+
+    final String producer;
+    final String model;
+    Integer yearOfProduction;
+    Double millage;
+    String color;
+
+    public Car(String producer, String model) {
+        this.producer = producer;
+        this.model = model;
+    }
+}
--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -7,9 +7,15 @@ public class Main {
         Animal cat = new Animal("felis");
         cat.name = "Futrzak";
 
+        Car car1 = new Car ("Renault", "Megane");
+        car1.yearOfProduction = 2011;
+        car1.millage = 120000.0;
+        car1.color = "grey";
+
         Human me = new Human();
         me.firstName = "Bartosz";
         me.lastName = "Lissowski";
+        me.car = car1;
 
         cat.feed();
         cat.takeForAWalk();
